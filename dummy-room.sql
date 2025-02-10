62d2e9ad23bb5d191f21e31c43427ff4d3baaede
mini0192 <parkgw9071@gmail.com>
2025-02-11 02:16:15 +0900

    모니터링 추가

--- dummy-room.sql
+++ dummy-room.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE loopInsert()
 BEGIN
     DECLARE i INT DEFAULT 1;
         
-    WHILE i <= 300 DO
+    WHILE i <= 1800 DO
         INSERT INTO room(id, room_name) VALUES(i, "test");
         INSERT INTO room_member(id, role, member_id, room_id) VALUES(i, "ADMIN", 1, i);
         SET i = i + 1;
